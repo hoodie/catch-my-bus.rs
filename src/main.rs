0d06956b76e4afec95383c152f006da1fbfa2974
Hendrik Sollich <hendrik@hoodie.de>
2015-06-22 13:59:58 +0200

    running in circles

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,21 @@ use notify_rust::Notification;
 
 fn main() {
     let stations = config::read_config();
-    for station in &stations{
-        let json = dvb::get_station_json(station);
-        let map = dvb::group_by_line(&json);
-        for (k, v) in map{
+    loop{
+        for station in &stations{
+            let json = dvb::get_station_json(station);
+            let map = dvb::group_by_line(&json);
+            for (k, v) in map{
 
-            let body = &format!("{}:\n  {}", &k,v.connect(", "));
-            Notification::new()
-                .appname(&format!("catch_my_bus {}", &k))
-                .icon("/home/hendrik/code/rust/catch_my_bus/Bushaltestelle.png")
-                .summary(&station)
-                .body(body).show_debug();
+                let body = &format!("{}:\n  {}", &k,v.connect(", "));
+                Notification::new()
+                    .appname(&format!("catch_my_bus {}", &k))
+                    .icon("/home/hendrik/code/rust/catch_my_bus/Bushaltestelle.png")
+                    .summary(&station)
+                    .body(body).show_debug();
+            }
         }
-
+        std::thread::sleep_ms(60_000);
     }
 }
 
